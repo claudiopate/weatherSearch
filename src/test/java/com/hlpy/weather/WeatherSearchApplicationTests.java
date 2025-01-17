525a2df9de1da49c58e723fe0645eea4bf75e9a8
Claudio Paternesi <claudio.paternesi@xfarm.ag>
2025-01-17 21:45:30 +0100

    first commit

---
+++ src/test/java/com/hlpy/weather/WeatherSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hlpy.weather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
